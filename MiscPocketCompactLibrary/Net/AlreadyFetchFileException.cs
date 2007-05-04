5ac5889150679665ff4f2593f0d1787f6380f677
hirano <devnull@localhost>
2007-05-04 10:08:23 +0000

    ファイルの追加し忘れを修正

---
+++ MiscPocketCompactLibrary/Net/AlreadyFetchFileException.cs
@@ -0,0 +1,18 @@
+﻿#region ディレクティブを使用する
+
+using System;
+
+#endregion
+
+namespace MiscPocketCompactLibrary.Net
+{
+    /// <summary>
+    /// すでにファイルをゲットしている場合の例外
+    /// </summary>
+    public class AlreadyFetchFileException : Exception
+    {
+        public AlreadyFetchFileException() : base()
+        {
+        }
+    }
+}
